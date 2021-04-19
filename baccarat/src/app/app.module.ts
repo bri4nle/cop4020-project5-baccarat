9d40bfc7f237bc97e5cfccd29bfaa9a01eb44b07
David Carbaugh <davidpcarbaugh@gmail.com>
2021-04-19 17:23:24 -0500

    adjusting styling for visual components to look more like the mockup

--- baccarat/src/app/app.module.ts
+++ baccarat/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PlayerBoardComponent } from './player-board/player-board.component';
 import { TieBetCardComponent } from './tie-bet-card/tie-bet-card.component';
+import { BankerBetCardComponent } from './banker-bet-card/banker-bet-card.component';
+import { PlayerBetCardComponent } from './player-bet-card/player-bet-card.component';
+import { HandComponent } from './hand/hand.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PlayerBoardComponent,
-    TieBetCardComponent
+    TieBetCardComponent,
+    BankerBetCardComponent,
+    PlayerBetCardComponent,
+    HandComponent
   ],
   imports: [
     BrowserModule,
---
+++ baccarat/src/app/player-bet-card/player-bet-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-player-bet-card',
+  templateUrl: './player-bet-card.component.html',
+  styleUrls: ['./player-bet-card.component.css']
+})
+export class PlayerBetCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
